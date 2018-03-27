81e68e712ffde57e0628e186cd2551c7390c95c8
Kazakuri <kazakuri@urus.ai>
2018-03-26 22:40:43 -0400

    Remove c++17 helper variable

--- src/utilities/singleton.h
+++ src/utilities/singleton.h
@@ -7,7 +7,7 @@ template <typename T, typename D = T>
 class Singleton : public QObject {
  private:
   friend D;
-  static_assert(std::is_base_of_v<T, D>, "T should be a base type for D");
+  static_assert(std::is_base_of<T, D>::value, "T should be a base type for D");
 
  public:
   static T &instance() {

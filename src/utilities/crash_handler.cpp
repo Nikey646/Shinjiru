1dfc963e7046e2b7f70cc0d04e467b29430c0c08
Kazakuri <kazakuri@urus.ai>
2018-05-29 19:44:15 -0400

    Remove breakpad from linux builds and add INSTALL.linux

--- src/utilities/crash_handler.cpp
+++ src/utilities/crash_handler.cpp
@@ -4,9 +4,7 @@
 #include <QtCore/QDir>
 #include <QtCore/QProcess>
 
-#if defined(Q_OS_LINUX)
-#include <breakpad/src/client/linux/handler/exception_handler.h>
-#elif defined(Q_OS_WIN32)
+#ifdef Q_OS_WIN32
 #include <breakpad/src/client/windows/handler/exception_handler.h>
 #endif
 
@@ -35,20 +33,15 @@ bool CrashHandlerPrivate::bReportCrashesToSystem = false;
 /************************************************************************/
 /* DumpCallback                                                         */
 /************************************************************************/
-#if defined(Q_OS_WIN32)
+#ifdef Q_OS_WIN32
 bool DumpCallback(const wchar_t* _dump_dir, const wchar_t* _minidump_id, void* context,
-                  EXCEPTION_POINTERS* exinfo, MDRawAssertionInfo* assertion, bool success)
-#elif defined(Q_OS_LINUX)
-bool DumpCallback(const google_breakpad::MinidumpDescriptor& md, void* context, bool success)
-#endif
-{
+                  EXCEPTION_POINTERS* exinfo, MDRawAssertionInfo* assertion, bool success) {
   Q_UNUSED(context);
-#if defined(Q_OS_WIN32)
   Q_UNUSED(_dump_dir);
   Q_UNUSED(_minidump_id);
   Q_UNUSED(assertion);
   Q_UNUSED(exinfo);
-#endif
+
   qDebug("BreakpadQt crash");
 
   /*
@@ -57,22 +50,17 @@ bool DumpCallback(const google_breakpad::MinidumpDescriptor& md, void* context, 
   */
   return CrashHandlerPrivate::bReportCrashesToSystem ? success : true;
 }
+#endif
 
 void CrashHandlerPrivate::InitCrashHandler(const QString& dumpPath) {
   if (pHandler != NULL) return;
 
-#if defined(Q_OS_WIN32)
+#ifdef Q_OS_WIN32
   std::wstring pathAsStr = (const wchar_t*)dumpPath.utf16();
   pHandler = new google_breakpad::ExceptionHandler(pathAsStr,
                                                    /*FilterCallback*/ 0, DumpCallback,
                                                    /*context*/
                                                    0, true);
-#elif defined(Q_OS_LINUX)
-  std::string pathAsStr = dumpPath.toStdString();
-  google_breakpad::MinidumpDescriptor md(pathAsStr);
-  pHandler = new google_breakpad::ExceptionHandler(md,
-                                                   /*FilterCallback*/ 0, DumpCallback,
-                                                   /*context*/ 0, true, -1);
 #endif
 }
 
